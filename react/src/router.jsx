a48c8b08d7ed000550204c20970d97d18fd8d410
thisiszackmio <sermonzackmio@gmail.com>
2023-08-11 14:38:53 +0800

    Add existing project files to Git

---
+++ react/src/router.jsx
@@ -0,0 +1,40 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Dashboard from "./views/Dashboard";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Navigate to="/" />
+            },
+            {
+                path: '/',
+                element: <Dashboard />
+            }
+        ]
+    },
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/register',
+                element: <Register />
+            }
+        ]
+    }
+
+])
+
+export default router;

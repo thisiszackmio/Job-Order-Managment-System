fc051ddb625eb6500916a5a5ece3003e952418d9
thisiszackmio <sermonzackmio@gmail.com>
2024-10-02 08:25:37 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/my-app/',
+    base: '/my-app/', // Adjust this depending on your app deployment
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -10,14 +10,26 @@ export default defineConfig({
         }),
     ],
     server: {
-      hmr: {
-        host: '20.20.2.1'
-      }
+        hmr: {
+            host: 'localhost', // Change to your actual development host if needed
+        }
     },
     resolve: {
-      alias: {
-        '@': '/src',
-      },
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+            '@': '/src',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+        chunkSizeWarningLimit: 1000, // Optional: Adjust this based on your app's needs
     },
 });

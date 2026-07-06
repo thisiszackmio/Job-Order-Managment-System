11c458b18606546640c3b6b0b8d0954999b5f87e
zackmiohaha <sermonzackmio@gmail.com>
2026-07-06 12:39:19 +0800

    update

--- react/src/views/dashboard.jsx
+++ react/src/views/dashboard.jsx
@@ -228,6 +228,7 @@ export default function Dashboard() {
         <div className="greet-section">
             <div className="greeting">
                 {getGreeting()} {currentUserName.gender == 'Male' ? "Sir":"Ma'am"} {APM && "APM"} {currentUserName.firstname}
+                Hi
             </div>
         </div>
 
